da27ffcfc101a346319e2c59a5c7f8869578d245
benoitgaujac <benoit.gaujac@gmail.com>
2018-11-09 10:41:05 +0000

    plot_functions gpu

--- plot_functions.py
+++ plot_functions.py
@@ -143,8 +143,8 @@ def save_train(opts, data_train, data_test,
                                 size=20, transform=ax.transAxes)
 
     ### The reconstruction loss curves
-    base = plt.cm.get_cmap('Vega10')
-    #base = plt.cm.get_cmap('tab10')
+    #base = plt.cm.get_cmap('Vega10')
+    base = plt.cm.get_cmap('tab10')
     color_list = base(np.linspace(0, 1, 10))
     ax = plt.subplot(gs[1, 1])
     total_num = len(losses_rec)
@@ -175,8 +175,8 @@ def save_train(opts, data_train, data_test,
                                 metric='correlation').fit_transform(np.concatenate((encoded,samples_prior),axis=0))
 
     plt.scatter(embedding[:num_pics, 0], embedding[:num_pics, 1],
-                #c=label_test[:num_pics], s=40, label='Qz test',cmap=discrete_cmap(10, base_cmap='tab10'))
-                c=label_test[:num_pics], s=40, label='Qz test',cmap=discrete_cmap(10, base_cmap='Vega10'))
+                c=label_test[:num_pics], s=40, label='Qz test',cmap=discrete_cmap(10, base_cmap='tab10'))
+                #c=label_test[:num_pics], s=40, label='Qz test',cmap=discrete_cmap(10, base_cmap='Vega10'))
     plt.colorbar()
     plt.scatter(embedding[num_pics:, 0], embedding[num_pics:, 1],
                             color='navy', s=10, marker='*',label='Pz')
