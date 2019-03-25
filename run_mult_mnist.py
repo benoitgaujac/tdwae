2a29b6d599867e5e2e71b2f8aaf90356a8300c80
benoitgaujac <benoit.gaujac@gmail.com>
2019-03-25 16:13:27 +0000

    run mult lambdas

--- run_mult_mnist.py
+++ run_mult_mnist.py
@@ -15,10 +15,9 @@ parser = argparse.ArgumentParser()
 # Args for experiment
 parser.add_argument("--mode", default='test',
                     help='mode to run [train/test/vizu]')
-parser.add_argument("--exp", default='mnist',
-                    help='dataset [mnist/cifar10/].'\
-                    ' celebA/dsprites Not implemented yet')
 parser.add_argument("--work_dir")
+parser.add_argument("--idx_lmba", type=int, default=0,
+                    help='lambda')
 parser.add_argument("--etype", default='gauss',
                     help='encoder type')
 parser.add_argument("--weights_file")
@@ -77,31 +76,32 @@ def main():
 
     # Experiments
     lambda_values = [0.00001,0.00005,0.0001,0.0005,0.001]
-    for lambda_scalar in lambda_values:
-        logging.error('Experiment lambda %d' % lambda_scalar)
-        # lambda Value
-        opts['lambda'].append(lambda_scalar / opts['zdim'][-1])
+    lambda_scalar = lambda_values[FLAGS.idx_lmba]
+    # for lambda_scalar in lambda_values:
+    logging.error('Experiment lambda %d' % lambda_scalar)
+    # lambda Value
+    opts['lambda'].append(lambda_scalar / opts['zdim'][-1])
 
-        # Create working directories
-        work_dir = FLAGS.work_dir + '_' + str(lambda_scalar)
-        work_dir = os.path.join(opts['method'],work_dir)
-        opts['work_dir'] = work_dir
-        utils.create_dir(work_dir)
-        utils.create_dir(os.path.join(work_dir, 'checkpoints'))
+    # Create working directories
+    work_dir = FLAGS.work_dir + '_' + str(lambda_scalar)
+    work_dir = os.path.join(opts['method'],work_dir)
+    opts['work_dir'] = work_dir
+    utils.create_dir(work_dir)
+    utils.create_dir(os.path.join(work_dir, 'checkpoints'))
 
-        # Dumping all the configs to the text file
-        with utils.o_gfile((work_dir, 'params.txt'), 'w') as text:
-            text.write('Parameters:\n')
-            for key in opts:
-                text.write('%s : %s\n' % (key, opts[key]))
+    # Dumping all the configs to the text file
+    with utils.o_gfile((work_dir, 'params.txt'), 'w') as text:
+        text.write('Parameters:\n')
+        for key in opts:
+            text.write('%s : %s\n' % (key, opts[key]))
 
-        #Reset tf graph
-        tf.reset_default_graph()
+    #Reset tf graph
+    tf.reset_default_graph()
 
-        # build WAE
-        wae = WAE(opts)
+    # build WAE
+    wae = WAE(opts)
 
-        # Training
-        wae.train(data, FLAGS.weights_file)
+    # Training
+    wae.train(data, FLAGS.weights_file)
 
 main()
