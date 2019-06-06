38db48c7c820fcb71bc6aaafcaa704590714edb2
benoitgaujac <benoit.gaujac@gmail.com>
2019-06-06 09:59:00 +0100

    cifar10 5l

--- run_cifar10_5l.py
+++ run_cifar10_5l.py
@@ -75,10 +75,13 @@ def main():
     # Model set up
     opts['nlatents'] = 5
     zdim_small = [26,22,18,14,10]
+    zdim_smallmiddl = [32,28,24,20,16]
     zdim_middl = [46,42,38,34,30]
     zdim_large = [64,60,56,52,48]
     if FLAGS.zdim=='small':
         opts['zdim'] = zdim_small
+    if FLAGS.zdim=='smallmiddl':
+        opts['zdim'] = zdim_smallmiddl
     elif FLAGS.zdim=='middle':
         opts['zdim'] = zdim_middl
     elif FLAGS.zdim=='large':
