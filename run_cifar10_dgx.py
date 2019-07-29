34d2d58070a262c361ba4dfd4139b12a581cecd0
benoitgaujac <benoit.gaujac@gmail.com>
2019-07-29 17:08:40 +0100

    kernel MMD

--- run_cifar10_dgx.py
+++ run_cifar10_dgx.py
@@ -74,18 +74,19 @@ def main():
 
     # Model set up
     opts['nlatents'] = 8
-    opts['zdim'] = [34, 30, 26, 22, 18, 14, 10, 64]
+    opts['zdim'] = [34, 30, 26, 22, 18, 14, 10, 256]
 
     # Penalty
     opts['pen'] = FLAGS.penalty
+    opts['mmd_kernel'] = 'RQ'
     opts['pen_enc_sigma'] = True
     opts['lambda_pen_enc_sigma'] = [0.000001,]*(opts['nlatents']-1)
-    opts['lambda_pen_enc_sigma'].append(0.000000001)
+    opts['lambda_pen_enc_sigma'].append(0.0000001)
     opts['pen_dec_sigma'] = False
     opts['lambda_pen_dec_sigma'] = [0.0005,]*opts['nlatents']
     opts['obs_cost'] = 'l2sq' #l2, l2sq, l2sq_norm, l1
     opts['latent_cost'] = 'l2sq_gauss' #l2, l2sq, l2sq_norm, l2sq_gauss, l1
-    # opts['lambda'] = [FLAGS.base_lmba**(i+1) / opts['zdim'][i+1] for i in range(opts['nlatents']-1)]
+    # opts['lambda'] = [FLAGS.base_lmba**(i+1) for i in range(opts['nlatents']-1)]
     opts['lambda'] = [FLAGS.base_lmba**(i/opts['nlatents']+1) for i in range(opts['nlatents']-1)]
     opts['lambda'].append(FLAGS.lmba)
     opts['lambda_schedule'] = 'constant'
@@ -98,7 +99,7 @@ def main():
     opts['e_arch'] = [FLAGS.enet_archi,]*opts['nlatents'] # mlp, dcgan, dcgan_v2, resnet
     opts['e_resample'] = ['down',None,None,None,'down',None,None,'down'] #None, down
     opts['e_nlayers'] = [3,]*opts['nlatents']
-    opts['e_nfilters'] = [96,]*opts['nlatents']
+    opts['e_nfilters'] = [128,]*opts['nlatents']
     opts['e_nonlinearity'] = 'leaky_relu' # soft_plus, relu, leaky_relu, tanh
     opts['e_norm'] = 'batchnorm' #batchnorm, layernorm, none
     opts['decoder'] = ['det','gauss','gauss','gauss','gauss','gauss','gauss','gauss','gauss','gauss','gauss','gauss','gauss','gauss','gauss'] # deterministic, gaussian
@@ -106,7 +107,7 @@ def main():
     opts['dconv_1x1'] = False
     opts['d_resample'] = ['up',None,None,None,'up',None,None,'up'] #None, up
     opts['d_nlayers'] = [3,]*opts['nlatents']
-    opts['d_nfilters'] = [96,]*opts['nlatents']
+    opts['d_nfilters'] = [128,]*opts['nlatents']
     opts['d_nonlinearity'] = 'relu' # soft_plus, relu, leaky_relu, tanh
     opts['d_norm'] = 'batchnorm' #batchnorm, layernorm, none
 
