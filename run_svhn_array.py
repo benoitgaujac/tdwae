36397100eac72d335bfd2848448c6cfe5f9b7d1f
benoitgaujac <benoit.gaujac@gmail.com>
2020-01-08 18:48:38 +0000

    svhn icml

--- run_svhn_array.py
+++ run_svhn_array.py
@@ -62,9 +62,9 @@ def main():
         opts['fid'] = False
 
     # Experiemnts set up
-    opts['epoch_num'] = 105
-    opts['print_every'] =  10*6043 #78125 #every 25 epochs
-    opts['lr'] = 0.0005
+    opts['epoch_num'] = 205
+    opts['print_every'] =  20*6043 #78125 #every 25 epochs
+    opts['lr'] = 0.0003
     opts['batch_size'] = 100
     opts['dropout_rate'] = 1.
     opts['rec_loss_resamples'] = 'encoder'
