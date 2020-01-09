7f54ef39f9d1d371360889c6cf25f1bddbd4e090
benoitgaujac <benoit.gaujac@gmail.com>
2020-01-09 10:28:30 +0000

    celebA icml

--- run_celeba_array.py
+++ run_celeba_array.py
@@ -61,7 +61,7 @@ def main():
 
     # Experiemnts set up
     opts['epoch_num'] = 200
-    opts['print_every'] = 10*3010 #3010 it/epoch
+    opts['print_every'] = 5*3010 #3010 it/epoch
     opts['lr'] = 0.0004
     opts['batch_size'] = 64
     opts['dropout_rate'] = 1.
@@ -84,11 +84,11 @@ def main():
     opts['pen'] = FLAGS.penalty
     opts['mmd_kernel'] = 'IMQ'
     opts['pen_enc_sigma'] = True
-    base_lmba = [0.005, 0.1, 0.5]
-    lmba = [0.1, 0.5, 1.]
+    base_lmba = [0.01, 0.05, 0.1, 0.5]
+    lmba = [base_lmba[i]**(8/3+1)*10**(j+1) for j in range(2) for i in range(len(base_lmba))]
     lmbas = list(itertools.product(base_lmba,lmba))
-    opts['lambda_pen_enc_sigma'] = [1.5,]*(opts['nlatents']-1)
-    opts['lambda_pen_enc_sigma'].append(0.5)
+    opts['lambda_pen_enc_sigma'] = [1.,]*(opts['nlatents'])
+    # opts['lambda_pen_enc_sigma'].append(0.5)
     opts['pen_dec_sigma'] = False
     opts['lambda_pen_dec_sigma'] = [0.0005,]*opts['nlatents']
     opts['obs_cost'] = 'l2sq' #l2, l2sq, l2sq_norm, l1
