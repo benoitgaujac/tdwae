6fe9d6718902ae182deae04c320de7a44189ed49
benoitgaujac <benoit.gaujac@gmail.com>
2020-01-16 10:02:11 +0000

    celeba icml

--- run_celeba_array.py
+++ run_celeba_array.py
@@ -61,7 +61,7 @@ def main():
 
     # Experiemnts set up
     opts['epoch_num'] = 205
-    opts['print_every'] = 20*3010 #3010 it/epoch
+    opts['print_every'] = 2*3010 #3010 it/epoch
     opts['lr'] = 0.0004
     opts['batch_size'] = 64
     opts['dropout_rate'] = 1.
@@ -75,8 +75,8 @@ def main():
     opts['vizu_embedded'] = False
 
     # Model set up
-    opts['nlatents'] = 8
-    opts['zdim'] = [7, 6, 5, 4, 3, 2, 1, 32]
+    opts['nlatents'] = 10
+    opts['zdim'] = [9, 8, 7, 6, 5, 4, 3, 2, 1, 32]
     # zdims = [[2, 1, 2, 1, 2, 16], [1, 3, 2, 1, 2, 16], [1, 2, 4, 2, 1, 8],[2, 1, 2, 1, 2, 16]]
     # opts['zdim'] = zdims[FLAGS.exp_id-1]
 
@@ -84,14 +84,17 @@ def main():
     opts['pen'] = FLAGS.penalty
     opts['mmd_kernel'] = 'IMQ'
     lmbas = []
+    base_lmba = [0.01,]
+    lmba = [0.0001, 0.001, 0.01]
+    lmbas += list(itertools.product(base_lmba,lmba))
     base_lmba = [0.05,]
-    lmba = [0.05, 0.1, 0.5]
+    lmba = [0.001, 0.01, 0.1]
     lmbas += list(itertools.product(base_lmba,lmba))
     base_lmba = [0.1,]
-    lmba = [0.1, 1, 5]
+    lmba = [0.01, 0.1, 1]
     lmbas += list(itertools.product(base_lmba,lmba))
     base_lmba = [0.5,]
-    lmba = [10, 50, 100]
+    lmba = [.1, 1., 10.]
     lmbas += list(itertools.product(base_lmba,lmba))
     base_lmba = [1.,]
     lmba = [10, 100, 500]
@@ -100,7 +103,7 @@ def main():
     opts['lambda'] = [lmbas[FLAGS.exp_id-1][0]**(i/3+1) for i in range(opts['nlatents']-1)]
     opts['lambda'].append(lmbas[FLAGS.exp_id-1][1])
     opts['pen_enc_sigma'] = True
-    opts['lambda_pen_enc_sigma'] = [2.5 * exp(-4. * i / 6.) for i in range(opts['nlatents'])]
+    opts['lambda_pen_enc_sigma'] = [2.5 * exp(-3.5 * i / 6.) for i in range(opts['nlatents'])]
     opts['pen_dec_sigma'] = False
     opts['lambda_pen_dec_sigma'] = [0.0005,]*opts['nlatents']
     opts['obs_cost'] = 'l2sq' #l2, l2sq, l2sq_norm, l1
@@ -113,17 +116,17 @@ def main():
     opts['encoder'] = [FLAGS.etype,]*opts['nlatents'] # deterministic, gaussian
     opts['e_arch'] = [FLAGS.enet_archi,]*opts['nlatents'] # mlp, dcgan, dcgan_v2, resnet
     opts['e_last_archi'] = ['conv',]*opts['nlatents'] # dense, conv1x1, conv
-    opts['e_resample'] = ['down',  'down',  None, None, 'down', None, None, 'down'] # None, down
+    opts['e_resample'] = ['down',  'down',  None, None, None, 'down', None, None, None, 'down'] # None, down
     opts['e_nlayers'] = [3,]*opts['nlatents']
-    opts['e_nfilters'] = [64, 96, 96, 96, 96, 128, 128, 128]
+    opts['e_nfilters'] = [64, 96, 96, 96, 96, 96, 128, 128, 128, 128]
     opts['e_nonlinearity'] = 'leaky_relu' # soft_plus, relu, leaky_relu, tanh
     opts['e_norm'] = 'batchnorm' #batchnorm, layernorm, none
     opts['decoder'] = ['det','gauss','gauss','gauss','gauss','gauss','gauss','gauss','gauss','gauss'] # deterministic, gaussian
     opts['d_arch'] =  [FLAGS.dnet_archi,]*opts['nlatents'] # mlp, dcgan, dcgan_mod, resnet
     opts['d_last_archi'] = ['conv',]*opts['nlatents'] # dense, conv1x1, conv
-    opts['d_resample'] = ['up',  'up',  None, None, 'up', None, None, 'up'] #None, up
+    opts['d_resample'] = ['up',  'up',  None, None, None, 'up', None, None, None, 'up'] #None, up
     opts['d_nlayers'] = [3,]*opts['nlatents']
-    opts['d_nfilters'] = [64, 96, 96, 96, 96, 128, 128, 128]
+    opts['d_nfilters'] = [64, 96, 96, 96, 96, 96, 128, 128, 128, 128]
     opts['d_nonlinearity'] = 'relu' # soft_plus, relu, leaky_relu, tanh
     opts['d_norm'] = 'batchnorm' #batchnorm, layernorm, none
 
