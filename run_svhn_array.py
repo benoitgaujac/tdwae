36cc739b0b1f2e1c93742416e95010d0737f0e2a
benoitgaujac <benoit.gaujac@gmail.com>
2020-01-17 10:21:14 +0000

    svhn

--- run_svhn_array.py
+++ run_svhn_array.py
@@ -63,7 +63,7 @@ def main():
 
     # Experiemnts set up
     opts['epoch_num'] = 205
-    opts['print_every'] =  10*9443 #extra: 9443it/epoch, cropped: 1144it/epoch
+    opts['print_every'] =  1*9443 #extra: 9443it/epoch, cropped: 1144it/epoch
     opts['lr'] = 0.0003
     opts['batch_size'] = 64
     opts['dropout_rate'] = 1.
@@ -86,25 +86,24 @@ def main():
     opts['pen'] = FLAGS.penalty
     opts['mmd_kernel'] = 'IMQ'
     lmbas = []
-    base_lmba = [0.05,]
-    lmba = [0.01, 0.05, 0.1]
-    lmbas += list(itertools.product(base_lmba,lmba))
-    base_lmba = [0.1,]
-    # lmba = [0.0001, 0.001, 0.01, 0.1]
-    lmba = [0.01, 0.05, 0.1]
-    lmbas += list(itertools.product(base_lmba,lmba))
     base_lmba = [0.5,]
-    # lmba = [0.001, 0.01, 0.1, 1.]
-    lmba = [0.5, 1., 5., 10., 50.]
+    # lmba = [5., 10., 50.]
+    lmba = [20., 40., 60., 80., 100.]
     lmbas += list(itertools.product(base_lmba,lmba))
+    base_lmba = [1.1,]
+    # lmba = [5., 10., 50.]
+    lmba = [50., 100., 1000.]
+    lmbas += list(itertools.product(base_lmba,lmba))
+    pen_sigma_coef = [5./6. , 4./6., 3./6]
+    lmbas = list(itertools.product(lmbas,pen_sigma_coef))
     # base_lmba = [0.01, 0.05, 0.1, 0.5]
     # lmba1 = [0.000001, 0.00001, 0.0001, 0.001, 0.01, 0.1]
     # lmbas = list(itertools.product(base_lmba1,lmba1))
-    opts['lambda'] = [lmbas[FLAGS.exp_id-1][0]**(i/1.+1) for i in range(opts['nlatents']-1)]
-    opts['lambda'].append(lmbas[FLAGS.exp_id-1][1])
+    opts['lambda'] = [lmbas[FLAGS.exp_id-1][0][0]**(i/1.+1) for i in range(opts['nlatents']-1)]
+    opts['lambda'].append(lmbas[FLAGS.exp_id-1][0][1])
     opts['pen_enc_sigma'] = True
-    # opts['lambda_pen_enc_sigma'] = [2.6 - 0.5*i for i in range(opts['nlatents'])]
-    opts['lambda_pen_enc_sigma'] = [2.5 * exp(-5. * i / 6.) for i in range(opts['nlatents'])]
+    # opts['lambda_pen_enc_sigma'] = [2.5 * exp(-5. * i / 6.) for i in range(opts['nlatents'])]
+    opts['lambda_pen_enc_sigma'] = [2.5 * exp(- lmbas[FLAGS.exp_id-1][1]* i) for i in range(opts['nlatents'])]
     opts['pen_dec_sigma'] = False
     opts['lambda_pen_dec_sigma'] = [0.0005,]*opts['nlatents']
     opts['obs_cost'] = 'l2sq' #l2, l2sq, l2sq_norm, l1
