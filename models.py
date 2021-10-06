fixed kl

models.py
@@ -95,10 +95,13 @@ class Model(object):
 
     def layerwise_kl(self, inputs, sigma_scale):
         # --- compute layer-wise KL(q(z_i|z_i-1,p(z_i|z_i+1))
-        _, enc_means, enc_Sigmas = self.encode(inputs, sigma_scale, False, reuse=True, is_training=False)
-        pz_samples = tf.convert_to_tensor(sample_gaussian(self.opts, self.pz_params, 'numpy', self.opts['batch_size']))
-        _, dec_means, dec_Sigmas = self.sample_x_from_prior(pz_samples, sigma_scale)
-        dec_means, dec_Sigmas = dec_means[::-1], dec_Sigmas[::-1]
+        _, enc_means, enc_Sigmas, _, dec_means, dec_Sigmas = self.forward_pass(
+                                    inputs, sigma_scale, False, reuse=True,
+                                    is_training=False)
+        # _, enc_means, enc_Sigmas = self.encode(inputs, sigma_scale, False, reuse=True, is_training=False)
+        # pz_samples = tf.convert_to_tensor(sample_gaussian(self.opts, self.pz_params, 'numpy', self.opts['batch_size']))
+        # _, dec_means, dec_Sigmas = self.sample_x_from_prior(pz_samples, sigma_scale)
+        # dec_means, dec_Sigmas = dec_means[::-1], dec_Sigmas[::-1]
         KL = []
         # latent layer up to N-1
         for n in range(len(enc_means)-1):

run_wae.py
@@ -95,7 +95,7 @@ def main():
     # model
     opts['model'] = FLAGS.model
     opts['encoder'] = [FLAGS.encoder,]*opts['nlatents']
-    opts['use_sigmoid'] = FLAGS.sigmoid
+    # opts['use_sigmoid'] = FLAGS.sigmoid
     opts['archi'] = [FLAGS.net_archi,]*opts['nlatents']
     opts['obs_cost'] = FLAGS.cost
     opts['lambda_schedule'] = FLAGS.lmba_schedule
@@ -105,11 +105,13 @@ def main():
     # lamba
     lambda_rec = [0.0001, 0.001, 0.01, 0.1]
     lamdba_match = [0.0001, 0.001, 0.01, 0.1]
-    schedule = ['constant',]
-    lmba = list(itertools.product(schedule,lambda_rec,lamdba_match))
+    schedule = ['constant','adaptive']
+    sigmoid = [False, True]
+    lmba = list(itertools.product(schedule, sigmoid, lambda_rec,lamdba_match))
     id = (FLAGS.id-1) % len(lmba)
-    sche, lrec, lmatch = lmba[id][0], lmba[id][1], lmba[id][2]
+    sche, sig, lrec, lmatch = lmba[id][0], lmba[id][1], lmba[id][2], lmba[id][3]
     opts['lambda_schedule'] = sche
+    opts['use_sigmoid'] = sig
     opts['lambda_init'] = [lrec*log(n+1.0001)/opts['zdim'][n] for n in range(0,opts['nlatents']-1)] + [lmatch/100,]
     opts['lambda'] = [lrec**(n+1)/opts['zdim'][n] for n in range(0,opts['nlatents']-1)] + [lmatch,]
     # opts['lambda'] = [lrec*exp(-1/(n+1))/opts['zdim'][n] for n in range(0,opts['nlatents']-1)] + [lmatch,]
@@ -133,7 +135,7 @@ def main():
     out_subdir = os.path.join(opts['out_dir'], opts['model'])
     if not tf.io.gfile.isdir(out_subdir):
         utils.create_dir(out_subdir)
-    out_subdir = os.path.join(out_subdir, 'l'+sche+'_sigmoid')
+    out_subdir = os.path.join(out_subdir, 'l'+sche+'_sig'+str(sig))
     if not tf.io.gfile.isdir(out_subdir):
         utils.create_dir(out_subdir)
     opts['exp_dir'] = FLAGS.res_dir